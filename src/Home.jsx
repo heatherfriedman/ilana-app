10972a5932950bfd6aed0da8c16f911d41c12a0d
Heather Friedman <hfried25@gmail.com>
2020-11-20 17:24:59 -0500

    added check if name and room are correctly filled out

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,19 +12,32 @@ const SignInForm = styled.form`
 export const Home = () => {
   const [roomName, setRoomName] = useState('');
   const [name, setName] = useState('');
+  const [nameFlag, setNameFlag] = useState(false);
+  const [roomFlag, setRoomFlag] = useState(false);
+
   const history = useHistory();
 
   const handleRoomNameChange = event => {
     setRoomName(event.target.value);
+    setRoomFlag(false);
   };
 
   const handleNameChange = event => {
     setName(event.target.value);
+    setNameFlag(false);
   };
 
   const handleSubmit = e => {
     e.preventDefault();
-    history.push(`/${name}/${roomName}`);
+    if (name === '') {
+      setNameFlag(true);
+    }
+    if (roomName === '') {
+      setRoomFlag(true);
+    }
+    if (name !== '' && roomName !== '') {
+      history.push(`/${name}/${roomName}`);
+    }
   };
 
   const JoinRoomButton = styled.input``;
@@ -33,15 +46,18 @@ export const Home = () => {
     <>
       <div>Ilana's App</div>
       <SignInForm onSubmit={handleSubmit}>
-        <label>What is your name?</label>
+        <label>Type in your name:</label>
         <input type="text" placeholder="name" onChange={handleNameChange}></input>
+        <div>Choose a room:</div>
         <select onChange={handleRoomNameChange}>
-          <option>Choose a Room</option>
+          <option>Choose a room</option>
           <option value="Language Exchange">Language Exchange</option>
           <option value="Recipe Exchange"> Recipe Exchange</option>
           <option value="Book Club">Book Club</option>
         </select>
         <JoinRoomButton type="submit" value="join room" />
+        {nameFlag && <div>Please input name</div>}
+        {roomFlag && <div>Please select a room</div>}
       </SignInForm>
     </>
   );
